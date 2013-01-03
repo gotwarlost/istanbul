b4114726954d5ae35b1597cc6f1d820c94422fdf
gotwarlost <kananthmail-github@yahoo.com>
2013-01-03 11:54:58 -0800

    Add a 100ms delay to command runs in tests to ensure output/errors are collected correctly

--- test/cli-helper.js
+++ test/cli-helper.js
@@ -124,16 +124,18 @@ function runCommand(command, args, envVars, callback) {
         process.stderr.write(data);
     });
     handle.on('exit', function (exitCode) {
-        out = out.split(/\r?\n/);
-        err = err.split(/\r?\n/);
-        callback({
-            succeeded: function () { return exitCode === 0; },
-            exitCode: exitCode,
-            stdout: function () { return out; },
-            stderr: function () { return err; },
-            grepOutput: grepper(out),
-            grepError: grepper(err)
-        });
+        setTimeout(function () {
+            out = out.split(/\r?\n/);
+            err = err.split(/\r?\n/);
+            callback({
+                succeeded: function () { return exitCode === 0; },
+                exitCode: exitCode,
+                stdout: function () { return out; },
+                stderr: function () { return err; },
+                grepOutput: grepper(out),
+                grepError: grepper(err)
+            });
+        }, 100);
     });
 }
 
