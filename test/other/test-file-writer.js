dbe358220ea72b46275889f9010256f51c2db357
gotwarlost <kananthmail-github@yahoo.com>
2013-03-31 15:11:41 -0700

    Fix async writer interface, fixes #38

--- test/other/test-file-writer.js
+++ test/other/test-file-writer.js
@@ -1,17 +1,35 @@
 /*jslint nomen: true */
+
 var path = require('path'),
     fs = require('fs'),
-    exists = fs.existsSync || path.existsSync,
     mkdirp = require('mkdirp'),
+    rimraf = require('rimraf'),
     FileWriter = require('../../lib/util/file-writer'),
     outputDir = path.resolve(__dirname, 'output'),
     FOO = path.resolve(outputDir, 'foo-written.js'),
-    BAR = path.resolve(outputDir, 'bar-written.js'),
     writer,
+    MAX_FILES = 1000,
     LINES = [ 'This is line 1', 'This is line 2', 'This is line 3' ];
 
 function testContents(test) {
     test.equal(LINES.join('\n') + '\n', fs.readFileSync(FOO, 'utf8'));
+    test.done();
+}
+
+function fileNames() {
+    var i,
+        ret = [];
+    for (i = 0; i < MAX_FILES; i += 1) {
+        ret.push(path.resolve(outputDir, 'file' + i + '.txt'));
+    }
+    return ret;
+}
+
+function testAllFiles(test) {
+    var files = fileNames();
+    files.forEach(function (file) {
+        test.equal(file + '\n', fs.readFileSync(file, 'utf8'));
+    });
     test.done();
 }
 
@@ -24,42 +42,25 @@ function battery(sync) {
         },
         tearDown: function (cb) {
             writer = null;
-            if (exists(FOO)) { fs.unlinkSync(FOO); }
-            if (exists(BAR)) { fs.unlinkSync(BAR); }
+            rimraf.sync(outputDir);
             cb();
         },
-        "should write file with a series of calls": function (test) {
-            writer.start(FOO);
-            LINES.forEach(function (line) { writer.write(line); writer.write('\n'); });
-            writer.end(FOO);
-            if (sync) {
-                testContents(test);
-            } else {
-                setTimeout(testContents.bind(null, test), 500);
-            }
-        },
         "should write file in callback mode": function (test) {
+            writer.on('done', testContents.bind(null, test));
             writer.writeFile(FOO, function (w) {
                 LINES.forEach(function (line) { w.println(line); });
             });
-            if (sync) {
-                testContents(test);
-            } else {
-                setTimeout(testContents.bind(null, test), 500);
-            }
+            writer.done();
         },
-        "should not allow end before start": function (test) {
-            test.throws(function () {
-                writer.end();
-            }, /Attempt to end a file without starting it/);
-            test.done();
-        },
-        "should not allow start when started": function (test) {
-            writer.start(BAR);
-            test.throws(function () {
-                writer.start(FOO);
-            }, /Attempt to start a new file before ending the previous one/);
-            test.done();
+        "should write 1000 files": function (test) {
+            var files = fileNames();
+            writer.on('done', testAllFiles.bind(null, test));
+            files.forEach(function (file) {
+                writer.writeFile(file, function (w) {
+                    w.println(file);
+                });
+            });
+            writer.done();
         }
     };
 }
