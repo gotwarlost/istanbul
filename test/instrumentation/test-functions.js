952024b47034677e70f2115b1531225d63f30db6
gotwarlost <kananthmail-github@yahoo.com>
2013-06-30 14:32:10 -0700

    Set initial cover count for function declarations as 1 and never increment, fixes #67

--- test/instrumentation/test-functions.js
+++ test/instrumentation/test-functions.js
@@ -73,6 +73,30 @@ module.exports = {
             verifier.verify(test, [ 10 ], 15, { lines: { 1: 1, 2: 1, 4: 0 }, branches: { 1: [ 0, 1 ]}, functions: { 1: 0 }, statements: { 1: 1, 2: 1, 3: 0 } });
             test.done();
         }
+    },
+
+    "with a function declared in an 'unreachable' place": {
+        setUp: function (cb) {
+            code = [
+                'function foo(x) {',
+                '   return bar(x);',
+                '   function bar(y) { return y * 2 }',
+                '}',
+                'output = args[0] < 2 ? 2: foo(args[0]);'
+            ];
+            verifier = helper.verifier(__filename, code);
+            cb();
+        },
+
+        "should not cover functions but should cover declarations": function (test) {
+            verifier.verify(test, [ 1 ], 2, { lines: { 1: 1, 2: 0, 3: 1, 5: 1 }, branches: { 1: [1, 0 ] }, functions: { 1: 0, 2: 0 }, statements: { 1: 1, 2: 0, 3: 1, 4: 0, 5: 1} });
+            test.done();
+        },
+
+        "should cover functions and declarations": function (test) {
+            verifier.verify(test, [ 10 ], 20, { lines: { 1: 1, 2: 1, 3: 1, 5: 1 }, branches: { 1: [ 0, 1 ]}, functions: { 1: 1, 2: 1 }, statements: { 1: 1, 2: 1, 3: 1, 4: 1, 5: 1 } });
+            test.done();
+        }
     }
 };
 
