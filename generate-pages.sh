699bc0c73780a8bb314e8eb1dcec33bfbfdff0a2
gotwarlost <kananthmail-github@yahoo.com>
2014-07-07 18:11:11 +0530

    Misc docgen changes

--- generate-pages.sh
+++ generate-pages.sh
@@ -1,7 +1,6 @@
+set -ex
 export PAGES_DIR=../istanbul-pages
-npm install
 npm test --coverage
-npm install -g yuidocjs
 mkdir -p public/apidocs
 yuidoc .
 rsync -rvt ./public/apidocs/ ${PAGES_DIR}/public/apidocs/
