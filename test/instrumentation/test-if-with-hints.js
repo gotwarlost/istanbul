ca48c4f5e882a090dc29a291bfcef4106ceb6e9b
Felix Gerschau <felix.gerschau@gmail.com>
2019-10-27 13:56:26 +0100

    Add ignore last-else hint

--- test/instrumentation/test-if-with-hints.js
+++ test/instrumentation/test-if-with-hints.js
@@ -135,7 +135,7 @@ module.exports = {
             setUp: function (cb) {
                 code = [
                     '// hint',
-                    'if (args[0] > args [1])',
+                    'if (args[0] > args[1])',
                     '   output = args[0];',
                     'else',
                     '   output = args[1];'
@@ -160,6 +160,110 @@ module.exports = {
                 test.equal(true, cov.statementMap[2].skip);
                 test.done();
             }
+        }
+    },
+    "with if-elseif-else": {
+        "as a statement": {
+            setUp: function(cb) {
+                code = [
+                    '// hint',
+                    'if (args[0] > args[1]) {',
+                    '   output = args[0];',
+                    '} else if (args[0] < args[1]) {',
+                    '   output = args[1];',
+                    '} else if (args[0] > args[1]) {',
+                    '   output = args[0];',
+                    '} else {',
+                    '   output = null;',
+                    '}',
+                ];
+                cb(null);
+            },
+            "should cover then and elseif path": function (test) {
+                // without hint
+                verifier = helper.verifier(__filename, code);
+                verifier.verify(
+                    test,
+                    [ 10, 11 ],
+                    11,
+                    {
+                      lines: { 2: 1, 3: 0, 4: 1, 5: 1, 6: 0, 7: 0, 9: 0 },
+                      branches: { 1: [ 0, 1 ], 2: [ 1, 0 ], 3: [ 0, 0 ] },
+                      functions: {},
+                      statements: { 1: 1, 2: 0, 3: 1, 4: 1, 5: 0, 6: 0, 7: 0 },
+                    }
+                );
+
+                // with hint ignoring last else
+                code[0] = '// istanbul ignore last-else';
+                verifier = helper.verifier(__filename, code);
+                verifier.verify(
+                    test,
+                    [ 10, 11 ],
+                    11,
+                    {
+                      lines: { 2: 1, 3: 0, 4: 1, 5: 1, 6: 0, 7: 0, 9: 1 },
+                      branches: { 1: [ 0, 1 ], 2: [ 1, 0 ], 3: [ 0, 0 ] },
+                      functions: {},
+                      statements: { 1: 1, 2: 0, 3: 1, 4: 1, 5: 0, 6: 0, 7: 0 },
+                    }
+                );
+                var cov = verifier.getFileCoverage();
+                test.equal(true, cov.branchMap[3].locations[1].skip);
+                test.equal(true, cov.statementMap[7].skip);
+                test.done();
+            }
+        }
+    },
+    "as a nested if statement": {
+        setUp: function(cb) {
+            code = [
+                '// hint',
+                'if (args[0] >= args[1]) {',
+                '    if ( args[0] === args[1]) {',
+                '        output = -1;',
+                '    } else {',
+                '        output = args[0];',
+                '    }',
+                '} else if (args[0] < args[1]) {',
+                '    output = args[1];',
+                '} else {',
+                '   output = null;',
+                '}',
+            ];
+            cb(null);
+        },
+        "should should ignore last else": function (test) {
+            verifier = helper.verifier(__filename, code);
+            verifier.verify(
+                test,
+                [ 10, 10 ],
+                -1,
+                {
+                  lines: { 2: 1, 3: 1, 4: 1, 6: 0, 8: 0, 9: 0, 11: 0 },
+                  branches: { 1: [ 1, 0 ], 2: [ 1, 0 ], 3: [ 0, 0 ] },
+                  functions: {},
+                  statements: { 1: 1, 2: 1, 3: 1, 4: 0, 5: 0, 6: 0, 7: 0 },
+                }
+            );
+
+            code[0] = '// istanbul ignore last-else';
+            verifier = helper.verifier(__filename, code);
+            verifier.verify(
+                test,
+                [ 10, 10 ],
+                -1,
+                {
+                  lines: { 2: 1, 3: 1, 4: 1, 6: 0, 8: 0,  9: 0, 11: 1 },
+                  branches: { 1: [ 1, 0 ], 2: [ 1, 0 ], 3: [ 0, 0 ] },
+                  functions: {},
+                  statements: { 1: 1, 2: 1, 3: 1, 4: 0, 5: 0, 6: 0, 7: 0 },
+                }
+            );
+            var cov = verifier.getFileCoverage();
+            test.equal(true, cov.statementMap[7].skip);
+            test.equal(true, cov.branchMap[3].locations[1].skip);
+            test.done();
         }
     }
 };
